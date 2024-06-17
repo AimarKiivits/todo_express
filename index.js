f0e00745a1be2b5b1e78380b921d1333a6cea66c
Aimar Kiivits <Aimarkiivits1@gmail.com>
2024-06-17 13:49:46 +0300

    Add error handling

--- index.js
+++ index.js
@@ -36,32 +36,41 @@ const writeFile = (filename, data) => {
 app.get('/', (req, res) => {
     readFile('./tasks.json')
         .then((tasks) => {
-            res.render('index', {tasks: tasks});
+            res.render('index', {tasks: tasks, error: null});
         });
 });
 
 app.post('/', (req, res) => {
-    readFile('./tasks.json')
-        .then((tasks) => {
-            let index
-            if (tasks.length === 0) {
-                index = 0;
-            } else {
-                index = tasks[tasks.length - 1].id + 1;
-            }
-            
-            const newTask = {
-                id: index,
-                task: req.body.task
-            }
+    let error = null
+    if (req.body.task.trim() === '') {
+        error = 'Task cannot be empty';
+        readFile('./tasks.json')
+            .then((tasks) => {
+                res.render('index', {tasks: tasks, error: error});
+            });
+    } else {
+        readFile('./tasks.json')
+            .then((tasks) => {
+                let index
+                if (tasks.length === 0) {
+                    index = 0;
+                } else {
+                    index = tasks[tasks.length - 1].id + 1;
+                }
+                
+                const newTask = {
+                    id: index,
+                    task: req.body.task
+                }
 
-            tasks.push(newTask);
+                tasks.push(newTask);
 
-            data = JSON.stringify(tasks, null, 2);
-            
-            writeFile('./tasks.json', data)    
-            res.redirect('/');
-        });
+                data = JSON.stringify(tasks, null, 2);
+                
+                writeFile('./tasks.json', data)    
+                res.redirect('/');
+            });
+    }
 });
 
 app.get('/delete-task/:taskId', (req, res) => {
