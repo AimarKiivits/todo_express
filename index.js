a772da661cdfa0b94d27a58eabfd62980ba66eab
Aimar Kiivits <Aimarkiivits1@gmail.com>
2024-06-17 11:44:25 +0300

    Write data to file

--- index.js
+++ index.js
@@ -6,17 +6,43 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return
+            }
+            const tasks = data.split('\n')
+            resolve(tasks);
+        });
+    });
+}
 
 app.get('/', (req, res) => {
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        const tasks = data.split('\n')
-        res.render('index', {tasks: tasks});
-    });
+    readFile('./tasks')
+        .then((tasks) => {
+            res.render('index', {tasks: tasks});
+        });
 });
+
+app.post('/', (req, res) => {
+    readFile('./tasks')
+        .then((tasks) => {
+            tasks.push(req.body.task);
+            const data = tasks.join('\n');
+            fs.writeFile('./tasks', data, (err) => {
+                if (err) {
+                    console.error(err);
+                    return
+                }
+                res.redirect('/');
+            });
+        });
+})
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
